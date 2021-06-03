5bb2ca4bb8fc031efa7685060eb6187a2d23ed86
Matt Chvatal <mdchvatal@gmail.com>
2021-06-03 19:07:32 -0400

    added App.js configuration for react-redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import MainComponent from './components/MainComponent';
 import './App.css';
+import { MemoryRouter } from 'react-router-dom';
+import {Provider} from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  
+  render() {
+      return (
+        <Provider store={store}>
+          <MemoryRouter>
+            <div>   
+              <MainComponent/>
+            </div>
+          </MemoryRouter>
+        </Provider>
+    );
+  }
 }
 
 export default App;
